fbe513020dea0a1af2ecdf27c81b56f8f1a54c2d
Rohan Panchal <rp285518@gmail.com>
2026-05-14 10:22:52 +0530

    added requirements file

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-b(@+6k2ych1-kq0_84w^s6xrrr4(q7l&&m4)sk!adohn0rn-!i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
